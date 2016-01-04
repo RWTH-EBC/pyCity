dfe7ce96b98a91e4a030451370017d6d2def9eba
JSchiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2016-01-04 15:02:34 +0100

    Update of setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
-from setuptools import setup
+from __future__ import print_function
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import codecs
+import os
+import sys
+
+import pycity
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
 
 setup(name='pycity',
       version='0.1.0',
@@ -8,5 +31,5 @@ setup(name='pycity',
       author_email='tschuetz@eonerc.rwth-aachen.de',
       license='GNU General Public License',
       packages=['pycity'],
-      setup_requires = ['numpy', 'pandas', 'sympy', 'nose', 'xlrd'],
-      install_requires = ['numpy', 'pandas', 'sympy', 'nose', 'xlrd'])
+      install_requires=['numpy', 'pandas', 'sympy', 'pytest', 'xlrd'],
+      cmdclass={'test': PyTest})

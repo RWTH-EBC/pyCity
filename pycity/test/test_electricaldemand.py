fb17dd93dcc1164b3f5524763ed0d03e10e7e549
JSchiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2016-01-04 14:32:29 +0100

    Add pytest for electricaldemand (still requires test for method=2)

---
+++ pycity/test/test_electricaldemand.py
@@ -0,0 +1,71 @@
+from __future__ import division
+
+import numpy as np
+
+import pycity.classes.demand.ElectricalDemand as ED
+import pytest
+from pycity.test.pycity_fixtures import create_environment
+
+
+class Test_ElectricalDemand(object):
+    """
+    Test class for pyCity electrical demand object.
+    """
+
+    def test_method0(self, create_environment):
+        #  energy comparison array
+        load_array = np.array([10, 10, 10, 10, 20, 20, 20, 20])
+
+        el_demand_object = ED.ElectricalDemand(create_environment, method=0,
+                                               loadcurve=load_array)
+
+        el_load_curve = el_demand_object.getDemand()
+
+        #  Compare arrays
+        np.testing.assert_equal(el_load_curve, load_array)
+
+    def test_method1(self, create_environment):  # Standard load profile
+        #  Generate electrical demand object
+        el_demand_object = ED.ElectricalDemand(create_environment, method=1,
+                                               profileType="H0",
+                                               annualDemand=3000)
+
+        #  Get space heating load curve (in W) per timestep
+        el_load_curve = el_demand_object.getDemand(currentValues=False)
+
+        #  Convert power to energy values (W to Ws)
+        el_en_demand_curve = create_environment.timer.timeDiscretization * \
+                             el_load_curve
+
+        #  Calculate electric energy demand value in kWh
+        el_en_demand_curve = el_en_demand_curve / (1000 * 3600)
+        el_en_demand_value = np.sum(el_en_demand_curve)
+
+        #  Check if sum of energy demand values is (almost) equal to input
+        assert abs(el_en_demand_value - 3000) <= 0.01
+
+    #  TODO: Continue over here
+    # def test_method2(self, create_environment):  # Stochastic load profile
+    #
+    #     #  Occupancy profile
+    #     occupancy_profile = np.ones((365 * 24 * 60,), dtype=np.int)
+    #
+    #     el_dem_stochastic = ED.ElectricalDemand(create_environment,
+    #                                             annualDemand=3000,
+    #                                             method=2,
+    #                                             total_nb_occupants=1,
+    #                                             randomizeAppliances=True,
+    #                                             lightConfiguration=10,
+    #                                             occupancy=occupancy_profile)
+    #
+    #     #  Get space heating load curve (in W) per timestep (1 minute)
+    #     el_load_curve = el_dem_stochastic.getDemand(currentValues=False)
+    #
+    #     #  Convert power to energy values (W to Ws)
+    #     el_en_demand_curve = create_environment.timer.timeDiscretization * \
+    #                          el_load_curve
+    #
+    #     #  Calculate electric energy demand value in kWh
+    #     el_en_demand_curve = el_en_demand_curve / (1000 * 3600)
+    #     el_en_demand_value = np.sum(el_en_demand_curve)
+    #     print(el_en_demand_value)
--- pycity/test/test_spaceheating.py
+++ pycity/test/test_spaceheating.py
@@ -7,34 +7,36 @@ import pycity.classes.demand.SpaceHeating as SpaceHeating
 from pycity.test.pycity_fixtures import create_environment
 
 
-def test_method0(create_environment):
-    #  energy comparison array
-    load_array = np.array([10, 10, 10, 10, 20, 20, 20, 20])
+class Test_SpaceHeating(object):
 
-    #  Space heating object
-    spaceheating = SpaceHeating.SpaceHeating(create_environment, method=0,
-                                             loadcurve=load_array)
+    def test_method0(self, create_environment):
+        #  energy comparison array
+        load_array = np.array([10, 10, 10, 10, 20, 20, 20, 20])
 
-    #  Return load curve of space heating object
-    space_heat_load_curve = spaceheating.getDemand(currentValues=False)
+        #  Space heating object
+        spaceheating = SpaceHeating.SpaceHeating(create_environment, method=0,
+                                                 loadcurve=load_array)
 
-    #  Compare arrays
-    np.testing.assert_equal(space_heat_load_curve, load_array)
+        #  Return load curve of space heating object
+        space_heat_load_curve = spaceheating.getDemand(currentValues=False)
+
+        #  Compare arrays
+        np.testing.assert_equal(space_heat_load_curve, load_array)
 
 
-def test_method1(create_environment):  # Standard load profile
+    def test_method1(self, create_environment):  # Standard load profile
 
-    #  Generate space heating object
-    spaceheating = SpaceHeating.SpaceHeating(create_environment, method=1,
-                                             livingArea=100,
-                                             specificDemand=150)
+        #  Generate space heating object
+        spaceheating = SpaceHeating.SpaceHeating(create_environment, method=1,
+                                                 livingArea=100,
+                                                 specificDemand=150)
 
-    #  Get space heating load curve (in W) per timestep
-    space_heat_load_curve = spaceheating.getDemand(currentValues=False)
-    #  Convert to energy demand values (in kWh)
-    th_energy_demand_curve = space_heat_load_curve * \
-                             create_environment.timer.timeDiscretization / \
-                             (1000 * 3600)
+        #  Get space heating load curve (in W) per timestep
+        space_heat_load_curve = spaceheating.getDemand(currentValues=False)
+        #  Convert to energy demand values (in kWh)
+        th_energy_demand_curve = space_heat_load_curve * \
+                                 create_environment.timer.timeDiscretization / \
+                                 (1000 * 3600)
 
-    #  Check if sum of energy demand values is (almost) equal to input
-    assert (np.sum(th_energy_demand_curve) - 150 * 100) <= 0.01
+        #  Check if sum of energy demand values is (almost) equal to input
+        assert abs(np.sum(th_energy_demand_curve) - 150 * 100) <= 0.01

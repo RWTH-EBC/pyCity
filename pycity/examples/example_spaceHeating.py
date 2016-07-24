a26d8e55cdb840c943e0ca51b634f4eefbb9de4c
Jan Schiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2016-07-24 19:42:50 +0200

    Add space heating demand curve usage with Modelica profiles (#138)

--- pycity/examples/example_spaceHeating.py
+++ pycity/examples/example_spaceHeating.py
@@ -7,6 +7,7 @@ Created on Fri May 22 11:29:11 2015
 """
 
 from __future__ import division
+import matplotlib.pyplot as plt
 
 import pycity.classes.demand.SpaceHeating as SpaceHeating
 
@@ -15,25 +16,47 @@ import pycity.classes.Weather
 import pycity.classes.Environment
 import pycity.classes.Prices
 
+
 def run_test():
     timer = pycity.classes.Timer.Timer()
     weather = pycity.classes.Weather.Weather(timer, useTRY=True)
     prices = pycity.classes.Prices.Prices()
 
-    environment = pycity.classes.Environment.Environment(timer, weather, prices)
+    environment = pycity.classes.Environment.Environment(timer, weather,
+                                                         prices)
 
+    #  Use standardized thermal load profile
+    #  #---------------------------------------
     hd_slp = SpaceHeating.SpaceHeating(environment,
-                                       method=1, # Standard load profile
-                                       livingArea=146,
-                                       specificDemand=166)
+                                       method=1,  # Standard load profile
+                                       livingArea=150,
+                                       specificDemand=100)
 
     results = hd_slp.get_power()
 
     print()
-    print("Heat demand: " + str(results))
+    print("Heat power curve: " + str(results))
 
-    import matplotlib.pyplot as plt
     plt.plot(hd_slp.loadcurve, label="SLP", color="b")
+    plt.show()
+
+    #  Use simulated profile
+    #  #---------------------------------------
+    sim_th_load = SpaceHeating.SpaceHeating(environment, method=3,
+                                            # Sim profile
+                                            livingArea=150,
+                                            specificDemand=100)
+
+    sim_th_loadcurve = sim_th_load.loadcurve
+
+    print('Thermal power load in W:', sim_th_loadcurve)
+    plt.plot(sim_th_loadcurve, label="Simulated power curve", color="b")
+    plt.show()
+
+    plt.plot(sim_th_loadcurve, label="Simulated power curve")
+    plt.plot(hd_slp.loadcurve, label="SLP")
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     #  Run program

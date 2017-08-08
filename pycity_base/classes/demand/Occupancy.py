dd919cdadf53f6d9e17f54401ac2f2c01948e848
JSchiefelbein <jschiefelbein@eonerc.rwth-aachen.de>
2017-08-08 15:27:51 +0200

    Integrate richardsonpy with imports #189

--- pycity_base/classes/demand/Occupancy.py
+++ pycity_base/classes/demand/Occupancy.py
@@ -6,38 +6,38 @@ occupancy profile.
 """
 
 from __future__ import division
-import os
-import numpy as np
-import random
+import copy
 
-import pycity_base.functions.occupancy_model as occupancy_model
-
+import richardsonpy.classes.occupancy as occ
+import pycity_base.functions.changeResolution as chres
 
 class Occupancy(object):
     """
     """
 
-    # Define class variables for transition_probability_matrixes and initial 
-    # occupancy 
-    tpm = {}
-
-    occ_start_states = {}
-    occ_start_states_loaded = False
-
-    type_weekday = ["wd", "we"]  # weekday, weekend
-
     def __init__(self, environment, number_occupants, initial_day=1,
+                 nb_days=365,
                  do_profile=True):
         """
-        This class loads all input data before 
-        
+        Constructor of occupancy object.
+
+        If occupancy profile is generated with do_profile == True, profile
+        is saved with timestep of 600 seconds on Occupancy.occupancy.
+        To return profile in other timestep, use method
+        get_occ_profile_in_curr_timestep.
+
         Parameters
         ----------
-        number_occupants : integer
-            How many active occupants live in this apartment?
-        initial_day : integer, optional
-            Initial day. 1-5 correspond to Monday-Friday, 6-7 to Saturday and 
+        environment : object
+            Environment of pyCity
+        number_occupants : int
+            Maximum number of occupants within apartment (range from 1 to 5)
+        initial_day : int, optional
+            Initial day. 1-5 correspond to Monday-Friday, 6-7 to Saturday and
             Sunday
+        nb_days : int, optional
+            Number of days, which should be used to generate profile
+            (default: 365)
         do_profile : bool, optional
             Defines, if user profile should be generated (default: True).
             If set to False, only number of occupants is saved and no
@@ -49,108 +49,62 @@ class Occupancy(object):
         assert number_occupants <= 5, ('Max. allowed number of occupants ' +
                                        'per apartment is 5')
 
+        assert nb_days > 0, 'Number of days must be larger than zero.'
+
+        assert initial_day in [1, 2, 3, 4, 5, 6, 7]
+
         self._kind = 'occupancy'
-        self.number_occupants = number_occupants
         self.environment = environment
-        self.occupancy = None  # Occupancy profile
+        self.number_occupants = number_occupants
         self.initial_day = initial_day
+        self.nb_days = nb_days
+        self.occupancy = None  # Occupancy profile
 
         if do_profile:
-            self.gen_occ_profile()
+            occupancy = occ.Occupancy(number_occupants=number_occupants,
+                                      initial_day=initial_day,
+                                      nb_days=nb_days,
+                                      do_profile=do_profile)
 
-    def gen_occ_profile(self):
+            occupancy.gen_occ_profile(nb_days=nb_days)
+
+            #  Save occupancy profile
+            self.occupancy = copy.copy(occupancy.occupancy)
+
+    def get_occ_profile_in_curr_timestep(self, timestep=None, int_con=False):
         """
-        Generate stochastic occupancy profile based on number of occupants
-        and weekday.
+        Returns occupancy profile in current timestep (as occupancy profile
+        is saved with 600 seconds timestep, per default)
+
+        Parameters
+        ----------
+        timestep : int, optional
+            Defines specific timestep to return occupancy profile in
+            (default: None). If None is chosen, returns profile with
+            timestep found in environment.timer
+        int_con : bool, optional
+            Defines, if output values should be converted into integer
+            numbers (conversion might lead to float numbers, depending
+            on chosen timesteps) (default: False)
+
+        Returns
+        -------
+        occ_profile : np.array
+            Numpy array holding number of persons, present within
+            zone
         """
-        src_path = os.path.dirname(os.path.dirname(os.path.dirname
-            (os.path.abspath(
-            __file__))))
-        folder_path = os.path.join(src_path, 'inputs',
-                                   'stochastic_electrical_load',
-                                   'constants')
-        if not Occupancy.occ_start_states_loaded:
-            # Load start states matrixes
-            Occupancy.occ_start_states_loaded = True
 
-            for weekday in self.type_weekday:
-                filename = str("occ_start_states_" + weekday + ".csv")
-                file_path = os.path.join(folder_path, filename)
-                temp = (np.loadtxt(file_path, delimiter=";")).tolist()
-                Occupancy.occ_start_states[weekday] = temp
+        occ_profile = copy.copy(self.occupancy)
 
-        if not (self.number_occupants, "wd") in list(Occupancy.tpm.keys()):
-            # Load transition probability matrixes
-            for weekday in self.type_weekday:
-                fname = str(
-                    "tpm" + str(
-                        self.number_occupants) + "_" + weekday + ".csv")
-                file_path = os.path.join(folder_path, fname)
-                temp = (np.loadtxt(file_path, delimiter=";")).tolist()
-                Occupancy.tpm[self.number_occupants, weekday] = temp
+        if timestep is None:
+            timestep = self.environment.timer.timeDiscretization
 
-        # Determine initial occupancy:
-        # Determine if the current day is a weekend-day
-        if self.initial_day <= 5:
-            self.weekend = False
-        else:
-            self.weekend = True
-        # Get starting states and starting probabilities
-        start_states = Occupancy.occ_start_states
-        start_probs = start_states[self.type_weekday[self.weekend]]
-        self.initial_occupancy = self._get_start_state(
-                                      start_probs[:][self.number_occupants])
+        occ_profile = chres.changeResolution(values=occ_profile,
+                                             oldResolution=600,
+                                             newResolution=timestep)
 
-        # Make a full year occupancy computation
-        occupancy = []
-        # Loop over all days
-        for i in range(self.environment.timer.totalDays):
-            if (i + self.initial_day) % 7 in (0, 6):
-                weekend = True
-            else:
-                weekend = False
+        if int_con:
+            for i in range(len(occ_profile)):
+                occ_profile[i] = int(round(occ_profile[i]))
 
-            occupancy.append(self._get_occupancy(weekend))
-
-        occupancy = np.array(occupancy)
-        self.occupancy = np.reshape(occupancy, occupancy.size)
-
-    def _get_start_state(self, start_probabilities):
-        """
-        Determine the active occupancy start state.
-        """
-        # Pick a random number to determine the start state
-        fRand = random.random()
-
-        # Reset the cumulative probability count
-        fCumulativeP = 0
-
-        # Return value
-        result = 0
-
-        # Loop construction
-        found = False  # Boolean variable instead of using break
-        i = 0
-
-        while not found:
-            fCumulativeP += start_probabilities[i]
-            if (fRand < fCumulativeP or i >= len(start_probabilities) - 1):
-                result = i
-                found = True
-            i += 1
-
-        return result
-
-    def _get_occupancy(self, weekend):
-        """
-        """
-        # Select appropriate transition probability matrix
-        tpm = self.tpm[self.number_occupants, self.type_weekday[weekend]]
-
-        # Compute occupancy for all required time steps
-        occupancy = occupancy_model.all_states(tpm, self.initial_occupancy)
-
-        # Make the last computed occupancy the new initial value
-        self.initial_occupancy = occupancy[-1]
-
-        return occupancy
+        return occ_profile

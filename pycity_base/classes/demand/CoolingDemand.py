d2f5dc32e93c5709f9ca7c45ab1d414358596222
MichaMans <michael.mans@hotmail.com>
2018-05-16 15:24:30 +0200

    #234 added coolingdemand class with first simple methods

--- pycity_base/classes/demand/CoolingDemand.py
+++ pycity_base/classes/demand/CoolingDemand.py
@@ -13,10 +13,42 @@ class CoolingDemand(pycity_base.classes.demand.Load.Load):
     Implementation of the cooling demand object
     """
 
-    def __init__(self, environment, loadcurve):
+    def __init__(self,
+                 environment,
+                 loadcurve=[]):
         """
         Parameters
         ----------
-        Test : Test
+        environment : Environment object
+            Common to all other objects. Includes time and weather instances
+
+        method : Integer, optional
+            - `0` : Provide load curve directly (for all timesteps!)
+
+        loadcurve : Array-like
+            Load curve for all investigated time steps
 
         """
+
+        src_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+        if method == 0:
+            super(CoolingDemand, self).__init__(environment, loadcurve)
+
+    def get_power(self, currentValues=True):
+        """
+        Return cooling power curve
+
+        Parameters
+        ----------
+        currentValues : bool, optional
+            Return only current values (True) or the entire load (False)
+            (default: True)
+
+        Return
+        ------
+        loadcurve : np.array
+            Cooling power curve
+        """
+        if self.method in (0, 1, 2, 3, 4):
+            return self._getLoadcurve(currentValues)

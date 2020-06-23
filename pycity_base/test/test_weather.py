6b501526b54e08de89baffe0320ebc65b69936fe
ss391347 <sebastian.schwarz1@rwth-aachen.de>
2020-06-23 13:24:35 +0200

    Added attribute weather_dataset_name to class Weather.

--- pycity_base/test/test_weather.py
+++ pycity_base/test/test_weather.py
@@ -16,8 +16,14 @@ class TestWeather():
 
         timer = ti.Timer()
 
-        weather = we.Weather(timer=timer)
+        weather = we.Weather(timer=timer, use_TRY=True, use_TMY3=False)
 
         weather.setLocation(location=(50.76, 6.07),
                             time_zone=1,
                             altitude=200)
+
+        assert(weather.weather_dataset_name == "TRY2010_05_Jahr")
+
+        weather = we.Weather(timer=timer, use_TRY=False, use_TMY3=True)
+
+        assert (weather.weather_dataset_name == "tmy3_744860_new_york_jfk_airport")
